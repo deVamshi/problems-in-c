1d2ba81878eff4f07176c3b97f1e10cd99482701
Vamshidhar <dev.vamshidhar@gmail.com>
2021-06-09 14:09:06 +0000

    finds if a word contains vowel

--- main.c
+++ main.c
@@ -1,72 +1,32 @@
-// #include <stdio.h>
-// #include <string.h>
-// int main(){
-//   char myString[200];
-
-
-//   scanf("%s", myString);
-
-//   for(int i = 0;i < strlen(myString);i++){
-
-//       for(int j = 0; j < strlen(myString) - i - 1; j++){
-//           if(myString[j] > myString[j+1]){
-//               char temp = myString[j];
-//               myString[j] = myString[j+1];
-//               myString[j+1] = temp;
-//           }
-//       }
-
-//   }
-
-//   for(int k = 0;k < strlen(myString); k++){
-//       printf("%c", myString[k]);
-//   }
-  
-
- 
-//   return 0;
-// }
-
-
 #include <stdio.h>
-#include <string.h>
+
+
 int main(){
 
-    int size;
+  char vowels[] = {'a', 'e', 'i', 'o','u'};
+  char word[10000];
+  scanf("%s", word);
+  int i = 0, j = 0;
 
-
-    printf("Enter Size of array: ");
-
-    scanf("%i", &size);
-
-
-  int arr[size];
-
-
-
-  for(int l = 0; l < size;l++){
-
-   scanf("%i", &arr[l]);
-      
-  }
-
-  for(int i = 0; i < size;i++){
-
-      for(int j = 0; j < size - i - 1; j++){
-          if(arr[j] > arr[j+1]){
-              int temp = arr[j];
-              arr[j] = arr[j+1];
-              arr[j+1] = temp;
-          }
-      }
-
-  }
-
-  for(int k = 0;k < size; k++){
-      printf("%i", arr[k]);
-  }
+  int hasVowel = 0;
   
 
- 
+  while(word[i]){
+    j = 0;
+    while(vowels[j]){
+      if(word[i] == vowels[j]){
+        hasVowel = 1;
+        break;
+      }
+      j++;
+    }
+    i++;
+  }
+
+  if(hasVowel){
+    printf("The word contains a vowel");
+  } else{
+    printf("The word doesn't contain a vowel");
+  }
   return 0;
 }
